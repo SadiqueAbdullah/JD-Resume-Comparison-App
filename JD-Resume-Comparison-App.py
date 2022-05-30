709fe970407ca87fa54a9dff2f7febe3c9282e37
SadiqueAbdullah <sadique2abdullah@gmail.com>
2022-05-30 11:48:08 +0530

    Uploaded files

---
+++ JD-Resume-Comparison-App.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#Import Libraries
+import pandas as pd
+import numpy as np
+import re
+
+
+# In[10]:
+
+
+pip install docx2txt 
+
+
+# In[11]:
+
+
+import docx2txt
+
+
+# In[12]:
+
+
+#Importing additional libraries for ML
+import nltk
+nltk.download(['wordnet', 'punkt', 'stopwords'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+
+r_data= docx2txt.process("Sadique_Abdullah_Resume_internship_Rdc.docx")
+
+
+# In[13]:
+
+
+# # Write a tokenization function to process your text data
+#
+def tokenize(text):
+    """
+    This function is to process the received messages(text) into usable form.
+    it normalizes, tokenize, removes stop-words, lemmatizes, and finally stores clean tokenized word in a list
+    """
+
+    # Text Normalization
+    text = re.sub("[^a-zA-Z0_9]", " ", str(text))
+
+    # tokenize text
+    tokens = word_tokenize(text)
+
+    # Removing stop words
+    stop = stopwords.words("english")
+    tokens = [w for w in tokens if w not in stop]
+
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+# In[14]:
+
+
+d= tokenize(r_data)
+print(d)
+
+
+# In[19]:
+
+
+count=len(d)
+print(count)
+dni = ['sadique', 'mohammad', 'abdullah', 'gmail', 'com', 'linkedin', 'github', 'sadiqueabdullah', '9', 'profile', 'summary',
+      'i', 'currently', 'pursuing', 'full', 'time','san', 'francisco', 'seeking','us','of', 'california', 'usa','0','stem',
+      'kalinga', 'institute', 'industrial', 'technology', 'bhubaneswar', 'india', 'b', 'tech','00', 'work','present',
+      'assistant', 'independent','0k','hindustan', 'aeronautics', 'limited','for', 'playing', 'vital', 'rounder', 'role',
+      'bronze','recc', 'honeywell', 'aerospace', 'tempe', 'bangalore', 'senior', 'principal', 'gold', 'being', 'first',]
+res = []
+for i in d:
+    if i not in res and i not in dni:
+        res.append(i)
+
+print(res)
+
+
+# In[16]:
+
+
+print(len(res))
+
+
+# In[ ]:
+
+
+# Importing Job description
+
