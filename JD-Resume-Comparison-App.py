21df964189d92b5cc69decec1f667ca2e59cd9ec
SadiqueAbdullah <sadique2abdullah@gmail.com>
2023-01-12 11:03:15 -0800

    Updated visualization code

--- JD-Resume-Comparison-App.py
+++ JD-Resume-Comparison-App.py
@@ -12,7 +12,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 resume = docx2txt.process("Sadique_Abdullah_Resume_internship_Rdc.docx")
 
 # # Tokenization function to process your text data
@@ -59,15 +59,24 @@ for i in resume_data:
         res.append(i)
 
 print(res)
-
 print('Number of unique words in uploaded Resume is ',len(res))
 
+# Function for printing unique list
+
+def unique(list):
+    uniq_lst = []
+    for x in list:
+        if x not in uniq_lst:
+            uniq_lst.append(x)
+    return uniq_lst
+
 # Importing Job description
+
 jd_data= docx2txt.process("JD.docx")
 jd= tokenize(jd_data)
 print("Total number of of words in JD file is " "", len(jd))
-
-count_jd=len(jd)
+jd_uniq = unique(jd)
+count_jd=len(jd_uniq)
 print("Number of unique words in JD file is " "",count_jd)
 
 # Number of matching words
@@ -77,14 +86,41 @@ def comparator(x,y):
         if i in y:
             result.append(i)
     return result
-print(comparator(jd,res))
+#print(comparator(jd,res))
+my_list = comparator(jd,res)
+
+# Creating the sorted list of  Key words after resume and Jd Comparison
+
+# create the counts dictionary
+counts = {}
+
+# iterate through the list and count the values
+for value in my_list:
+    if value in counts:
+        counts[value] += 1
+    else:
+        counts[value] = 1
+
+# create the table list
+table = []
+
+# add the values and counts to the table list as tuples
+for key, value in counts.items():
+    table.append((key, value))
+
+# sort the table list by the count value
+table = sorted(table, key=lambda x: x[1], reverse= True)
+
+# print the table
+for value, count in table:
+    print(f"{value}: {count}")
+
 print("Count of words matching while comparing resume and JD is ",len(comparator(jd,res)))
 
 # Percent of match between JD and resume#
 print(f'Current JD and Resume match is {len(comparator(jd,res))/ len(jd)*100:.2f}%')
 
-# Refernce plot for word counts
+# Reference plot for word counts
 plt.figure(figsize=(10,5))
-val = range(1,len(comparator(jd,res))+1)
-plt.barh(comparator(jd,res),range(1,len(comparator(jd,res))+1))
+sns.countplot(y=my_list)
 plt.show()
